5297bcedfa6f7d7b2175c96fb8be55b1240e84d4
Sleepless Dog <di.burmistrov@gmail.com>
2026-01-15 00:52:00 +0100

    score_screen_clean

--- app/src/main/java/com/sleeplessdog/matchthewords/score/models/Award.kt
+++
@@ -1,8 +0,0 @@
-package com.sleeplessdog.matchthewords.score.models
-
-data class Award(
-    val title: String = "",
-    val description: String = "",
-    val imagePath: Int = 0,
-    val isLocked: Boolean = true,
-)
