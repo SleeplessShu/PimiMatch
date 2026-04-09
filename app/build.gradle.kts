6d27b0ccfb1fc6e45be05afedf43c8a441b317a6
Sleepless Dog <di.burmistrov@gmail.com>
2026-04-09 22:46:54 +0200

    1.010->1.011

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sleeplessdog.pimi"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.010"
+        versionCode = 11
+        versionName = "1.011"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
